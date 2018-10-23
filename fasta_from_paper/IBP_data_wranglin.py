9fb8dd5d368a2a7a92276ce732d06669fd3aa64e
wimthiels <32536010+wimthiels@users.noreply.github.com>
2018-10-23 09:17:11 +0200

    results from the automated pipeline

--- fasta_from_paper/IBP_data_wranglin.py
+++ fasta_from_paper/IBP_data_wranglin.py
@@ -6,16 +6,20 @@ Created on 16 Oct 2018
 import pandas as pd
 import re,os
 
-save_dir = r'./'
+#save_dir = r'F:\PROJECT\DataWrangling'
+save_dir = r''
 sub_dir = ''
 f_output_name = 'bed_v1'
 f_input_name = 'GSE75661_7.5k_collapsed_counts.txt'
-output_path = os.path.join(save_dir, sub_dir, f_output_name +'.csv')
-input_path = os.path.join(save_dir, sub_dir, f_input_name)
-df_7_5K_data = pd.read_csv(input_path, sep='\t')
+output_path= os.path.join(save_dir,sub_dir,f_output_name +'.csv')
+input_path = os.path.join(save_dir,sub_dir,f_input_name)
+df_7_5K_data = pd.read_csv(input_path,sep='\t')
 
-#this array determines the order of the columns (besides the names)
-ls_columns = ["oligo", "chrom", "start", "end"]
+ls_columns = ["oligo",   #this array determines the order of the columns (besides the names)
+           "chrom",
+           "start",
+           "end"
+           ]
 
 l_oligo = []
 l_chrom = []
@@ -27,7 +31,6 @@ l_oligo_rs = []
 p1 = re.compile('^chr')
 p2 = re.compile('[:_]')
 for oligo in df_7_5K_data.Oligo:
-    #print(oligo)
     m = p1.match(oligo)
     if m:
         l_oligo_split = p2.split(oligo)
@@ -37,13 +40,13 @@ for oligo in df_7_5K_data.Oligo:
         l_start.append(nb_startpos)
         l_end.append(nb_startpos+150)
     else:
-        l_oligo_split = oligo.split('_')
-        l_oligo.append(oligo)
-        l_chrom.append(l_oligo_split[0])
-        l_start.append(0)
-        l_end.append(0)
-        
-        l_oligo_rs.append(l_oligo_split[0])
+          l_oligo_split = oligo.split('_')
+#         l_oligo.append(oligo)
+#         l_chrom.append(l_oligo_split[0])
+#         l_start.append(0)
+#         l_end.append(0)
+#         
+          l_oligo_rs.append(l_oligo_split[0])
 
 d_df = {}  # the dict that will be used to create the dataframe  
 d_df[ls_columns[0]] = l_oligo
@@ -51,10 +54,10 @@ d_df[ls_columns[1]] = l_chrom
 d_df[ls_columns[2]] = l_start
 d_df[ls_columns[3]] = l_end
 data=pd.DataFrame(d_df)
-data.to_csv(output_path, columns=ls_columns, header=True)
+data.to_csv(output_path,columns=ls_columns,header=True)
 
 f_output_name = 'rs_list'
-output_path = os.path.join(save_dir, sub_dir, f_output_name +'.txt')
+output_path= os.path.join(save_dir,sub_dir,f_output_name +'.txt')
 with open(output_path, 'w') as f:
     for item in l_oligo_rs:
         f.write("%s\n" % item)
