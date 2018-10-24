12decdc22ef03970c65195fa16c1b09eb949f71d
wimthiels <32536010+wimthiels@users.noreply.github.com>
2018-10-24 19:31:51 +0200

    the bash pipeline

--- Get_BED_FASTA/get_bed_fasta.sh
+++ Get_BED_FASTA/get_bed_fasta.sh
@@ -1,6 +1,6 @@
 #get second excel file (supplementary data 2) from https://www.nature.com/articles/ncomms7683#supplementary-information
 #save the excel file as a tab-separated file : ncomms7683-s3.txt
-#to get the fasta sequences you need the hg19 fasta file in your folder, and change the headers via
+#to get the fasta sequences you need the hg19 fasta file in your folder
 
 
 sed 1,3d ncomms7683-s3.txt  | awk '$12 = /I_reg/ { print $8,"\t",$9,"\t" $10 }' | tr -d ' ' |uniq > I_reg.bed
