656413d49bff12346970d3dfced9b0cbb782cc91
wimthiels <32536010+wimthiels@users.noreply.github.com>
2018-10-24 19:04:06 +0200

    Add files via upload

---
+++ Get_BED_FASTA/get_bed_fasta.sh
@@ -0,0 +1,14 @@
+#get second excel file (supplementary data 2) from https://www.nature.com/articles/ncomms7683#supplementary-information
+#save the excel file as a tab-separated file : ncomms7683-s3.txt
+#to get the fasta sequences you need the hg19 fasta file in your folder, and change the headers via
+
+
+sed 1,3d ncomms7683-s3.txt  | awk '$12 = /I_reg/ { print $8,"\t",$9,"\t" $10 }' | tr -d ' ' |uniq > I_reg.bed
+sed 1,3d ncomms7683-s3.txt  | awk '$12 = /P_reg/ { print $8,"\t",$9,"\t",$10 }' | tr -d ' ' |uniq > P_reg.bed
+
+#change the headers in the hg19 fasta file to match the bed file
+# sed -i -E 's/>NC_0.*chromosome ([^,]*),.*/>chr\1/' GCF_000001405.25_GRCh37.p13_genomic.fna
+
+#get the fasta from the bed files
+bedtools getfasta -fi GCF_000001405.25_GRCh37.p13_genomic.fna -bed I_reg.bed  -fo  I_reg.fna
+bedtools getfasta -fi GCF_000001405.25_GRCh37.p13_genomic.fna -bed P_reg.bed  -fo  P_reg.fna
