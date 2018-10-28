d205e8fb285483326b87870f48e84f204c1fb4ee
Boris Shilov <21023619+borisofmuscovy@users.noreply.github.com>
2018-10-28 18:25:23 +0100

    comment over several lines

--- fastaToBed.py
+++ fastaToBed.py
@@ -1,7 +1,11 @@
 import sys
 
-# This script can be used to convert the positive and negative example files created by Wim to the bed formats as used by i-cisTarget. Use the python script in command line and put the target fasta file as first argument. Script will create an output file called "pos_ex.bed" i.d. a bed file with the positive examples of enhancer sequences.
-
+'''
+This script can be used to convert the positive and negative example files created by Wim
+to the bed formats as used by i-cisTarget. 
+Use the python script in command line and put the target fasta file as first argument. 
+Script will create an output file called "pos_ex.bed" i.d. a bed file with the positive examples of enhancer sequences.
+'''
 with open("pos_ex.bed", "a") as bed:
     with open(sys.argv[1], 'r') as handle:
         for line in handle:
