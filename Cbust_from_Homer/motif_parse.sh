cbca845ce3fabfcbdb503c38f98f68b2540f432f
wimthiels <32536010+wimthiels@users.noreply.github.com>
2018-11-06 00:07:07 +0100

    Add files via upload

--- Cbust_from_Homer/motif_parse.sh
+++ Cbust_from_Homer/motif_parse.sh
@@ -9,4 +9,6 @@ do
 	cbust -g20 -l -c 0 -m 0 -f 1 matrix_temp I_reg.fna > cbustOut/Homer_PvsI_Known_"${filename}"_cbustOut 
 done
 
-find /cbustout -name *cbustOut -exec cat {} + > Homer_PvsI_Known_TOTAL_cbustOut
+cd cbustout
+cat * > Homer_PvsI_Known_TOTAL_cbustOut
+#find /cbustout/ -name *cbustOut -exec cat {} + > Homer_PvsI_Known_TOTAL_cbustOut
