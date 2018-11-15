458224f64bc4640ac599471d14411f23499e0935
Boris Shilov <21023619+borisofmuscovy@users.noreply.github.com>
2018-11-15 16:43:05 +0100

    started work on integrating lucas' code for reading f1 cbust files

--- cbust_result.py
+++ cbust_result.py
@@ -20,6 +20,73 @@ class cbust_result:
         # this seems to consist of only using until the first space as the motif name.
         # we use the same logic in dict keys
 
+    def from_f3(cls, f3_output_filepath):
+
+    def from_f1(cls, f1_output_filepath):
+        # ASSUMPTIONS: ONE SECTION IN CBUST OUTPUT PER MOTIF, EVERY REGION IS SCORED FOR EVERY MOTIF
+        # GIVE P OR I AS SECOND ARGUMENT AFTER INPUT FILE (P FIRST)
+        import sys
+        motifs = ['id', 'class']
+        current_motif = ''
+        matrix = []
+        count = 0
+
+        classi = sys.argv[2]
+
+        with open(sys.argv[1], 'r') as handle:
+            for line in handle:
+                if line.startswith('>'):
+                    splt1 = line[1:].split(":")
+                    splt2 = splt1[1].split("-")
+
+                    chr = splt1[0]
+                    start = splt2[0]
+
+                if line.startswith('#'):
+                    motif = line[1:].split()[3]
+
+                    # NEW MOTIF STARTED
+                    if motif != current_motif:
+                        motifs.append(motif)
+                        current_motif = motif
+                        count = 0
+
+                # NEW ENTRY STARTED
+                if line[0].isdigit():
+                    split = line.split('\t')
+                    score = split[0]
+
+                    # relative start and end
+                    rel_start = split[1]
+                    rel_end = split[2]
+
+                    chr_start = int(start) + int(rel_start)
+                    chr_end = int(start) + int(rel_end)
+
+                    id = chr + ":" + str(chr_start) + "-" + str(chr_end)
+
+                    try:
+                        if matrix[count][0] != id:
+                            sys.stdout.write(current_motif)
+                            sys.stdout.write("ERROR")
+                        else:
+                            matrix[count].append(score)
+                    except:
+                        matrix.append([id, classi, score])
+
+                    count += 1
+
+        with open("FM.bed", "a") as FM:
+            if classi == 'P':
+                for motf in motifs:
+                    FM.write(motf + "\t")
+                FM.write("\n")
+            for entry in matrix:
+                FM.write(entry[0])
+                for score in entry[1:]:
+                    FM.write("\t" + score)
+                FM.write("\n")
+
     def calculate_reliable_motif_dict(self, motif_threshold, cluster_threshold):
         '''
         Getter for the reliable motif dictionary.
