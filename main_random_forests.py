4a70f96c8364031497fbb6178a6b81d4b81d92ac
Boris Shilov <21023619+borisofmuscovy@users.noreply.github.com>
2018-11-22 17:25:32 +0100

    first model

--- main_random_forests.py
+++ main_random_forests.py
@@ -1,6 +1,7 @@
 import cbust_result as cb
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from fancyimpute import IterativeImputer
 import numpy as np
 import pandas as pd
 
@@ -8,5 +9,21 @@ np.random.seed(100)
 feature_matrix = pd.read_table("feature_matrix.csv", sep=",", index_col=0)
 feature_matrix.head()
 
-train, validate, test = np.split(feature_matrix.sample(frac=1),
-                                 [int(.6*len(feature_matrix)), int(.8*len(feature_matrix))])
+feature_matrix_zeroed = feature_matrix.fillna(value=0)
+
+# 60, 20, 20
+train, validate, test = np.split(feature_matrix_zeroed.sample(frac=1),
+                                 [int(.6*len(feature_matrix_zeroed)), int(.8*len(feature_matrix_zeroed))])
+
+features = train.columns[1:]
+y = train.loc[:,'_label']
+
+classifier1 = RandomForestClassifier(n_jobs=2, random_state=0)
+classifier1.fit(train[features], y)
+
+
+predictions = classifier1.predict(test[features])
+# see how many incorrect classifications we do
+print(pd.crosstab(test['_label'], predictions, rownames=['Actual'], colnames=['Predicted']))
+# print a matrix of tuples of feature names and feature importances
+print(list(zip(train[features], classifier1.feature_importances_)))
