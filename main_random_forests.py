33a23e177f03176ce24d222d545756599a06a224
Boris Shilov <21023619+borisofmuscovy@users.noreply.github.com>
2018-11-27 11:07:23 +0100

    started report file - open and edit with RStudio!

--- main_random_forests.py
+++ main_random_forests.py
@@ -1,5 +1,6 @@
 import cbust_result as cb
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import precision_recall_curve
 from sklearn.model_selection import train_test_split
 from fancyimpute import IterativeImputer
 import matplotlib.pyplot as plt
@@ -26,14 +27,15 @@ classifier1.fit(train[features], y)
 predictions = classifier1.predict(test[features])
 # see how many incorrect classifications we do
 print(pd.crosstab(test['_label'], predictions, rownames=['Actual'], colnames=['Predicted']))
+precision_recall_curve(test['_label'], predictions)
 # print a matrix of tuples of feature names and feature importances
 list(zip(train[features], classifier1.feature_importances_))
 plt.plot( classifier1.feature_importances_)
-plt.show()
+#plt.show()
 # get the most important motifs for the random forest
 print(features[np.nonzero(classifier1.feature_importances_ > 0.006)])
 # 1-DVTGASTCAB seems to correspond to HIF1b(HLH)
 # which is a known cardiac myocyte motif http://www.ag-rehli.de/EnhancerMotifs/CL_0000746_known.html
 # Fra1(bZIP) is a known regulator of cell proliferation
 # Sox2(HMG) is an embryonic development regulator and cell fate determinant
-# DOF5.7 is some sort of zinc finger protein in plants
+# DOF5.7 is some sort of zinc finger protein in plants - actually SOX10
