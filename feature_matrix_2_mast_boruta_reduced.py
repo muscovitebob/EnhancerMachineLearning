da6651b48561f570768b2db8ea900ff037abf4e9
zixuanxie <37243570+zixuanxie@users.noreply.github.com>
2018-12-12 05:40:34 +0800

    Update feature_matrix_2_mast_boruta_reduced.py

--- feature_matrix_2_mast_boruta_reduced.py
+++ feature_matrix_2_mast_boruta_reduced.py
@@ -1,3 +1,10 @@
+
+#references:
+#https://www.kaggle.com/tilii7/boruta-feature-elimination/notebook
+#https://github.com/scikit-learn-contrib/boruta_py
+#https://www.kaggle.com/kanncaa1/roc-curve-with-k-fold-cv
+
+#Boruta feature selection:
 from __future__ import print_function
 import pandas as pd
 import numpy as np
@@ -6,17 +13,17 @@ from sklearn.ensemble import RandomForestClassifier
 from boruta import BorutaPy
 from scipy import interp
 import matplotlib.pyplot as plt
-from sklearn import svm, datasets
 from sklearn.metrics import roc_curve, auc
 from sklearn.model_selection import StratifiedKFold
 
-ftrm= pd.read_csv('feature_matrix_2_mast_reduced.csv').fillna(value=0)
+# Import origin train set
+ftrm= pd.read_csv('FM_mast_reduced_train.csv').fillna(value=0)
 ftrm_id = ftrm['id'].values
 X = ftrm.drop(['id','_label'], axis=1).values
 y = ftrm['_label'].values
 
-rfc=RandomForestClassifier(n_estimators=300,max_depth=None,min_samples_split=2)
-boruta_selector = BorutaPy(rfc, n_estimators='auto', verbose=2)
+rfc=RandomForestClassifier(n_jobs=-1,class_weight='balanced',n_estimators=1000)
+boruta_selector = BorutaPy(rfc, n_estimators=5, verbose=2)
 boruta_selector.fit(X, y)
 # number of selected features
 print ('\n Number of selected features:')
@@ -29,42 +36,36 @@ print ('\n Top %d features:' % boruta_selector.n_features_)
 print (feature_df.head(boruta_selector.n_features_))
 feature_df.to_csv('boruta-feature-ranking-mast.csv', index=False)
 
+# Get the reduced train set
+ftrm= pd.read_csv('FM_mast_reduced_train.csv').fillna(value=0)
 selected = ftrm.drop(['id','_label'], axis=1).columns[boruta_selector.support_]
 ftrm = ftrm[selected]
 ftrm['id'] = ftrm_id
 ftrm['_label'] = y
 ftrm = ftrm.set_index('id')
-ftrm.to_csv('boruta_filtered-matrix.csv', index_label='id')
+train_reduced=ftrm
+train_reduced.to_csv('feature_matrix_2_mast_boruta_train_reduced.csv', index_label='id')
+train = pd.read_csv('feature_matrix_2_mast_boruta_train_reduced.csv').fillna(value=0)
+X_train = train.drop(['id','_label'], axis=1).values
+y_train = train['_label']
 
-#ROC cureve
-data = pd.read_csv('boruta_filtered-matrix.csv').fillna(value=0)
-X = data.drop(['id','_label'], axis=1).values
-y = data['_label'].values
-forest=RandomForestClassifier(n_estimators=300,max_depth=None,min_samples_split=2)
-cv = StratifiedKFold(n_splits=10,shuffle=False)
-tprs = []
-aucs = []
-mean_fpr = np.linspace(0,1,100)
-i = 1
 
-for train,test in cv.split(X,y):
-    prediction = forest.fit(X[train],y[train]).predict_proba(X[test])
-    fpr, tpr, t = roc_curve(y[test], prediction[:, 1])
-    tprs.append(interp(mean_fpr, fpr, tpr))
-    roc_auc = auc(fpr, tpr)
-    aucs.append(roc_auc)
-    plt.plot(fpr, tpr, lw=2, alpha=0.3, label='ROC fold %d (AUC = %0.2f)' % (i, roc_auc))
-    i= i+1
+# Get the reduced test set
+test = pd.read_csv('FM_orig.csv').fillna(value=0)
+X_test = test[selected]
+y_test = test['_label'].values
 
+# Train the random forest again on the reduced train set to get a model and then predict the test set
+forest=RandomForestClassifier(n_jobs=-1,class_weight='balanced',n_estimators=1000)
+pred_prob = forest.fit(X_train,y_train).predict_proba(X_test)
+fpr, tpr, t = roc_curve(y_test, pred_prob[:,1])
+roc_auc = auc(fpr, tpr)
+print ("\n Area under ROC curve: %0.3f " % (roc_auc))
+
+
+# Plot ROC curve without cross validation
+plt.plot(fpr, tpr, lw=2, alpha=0.3, label='ROC fold (AUC = %0.3f)' % (roc_auc))
 plt.plot([0,1],[0,1],linestyle = '--',lw = 2,color = 'black')
-mean_tpr = np.mean(tprs, axis=0)
-mean_auc = auc(mean_fpr, mean_tpr)
-plt.plot(mean_fpr, mean_tpr, color='blue', label=r'Mean ROC (AUC = %0.2f )' % (mean_auc),lw=2, alpha=1)
-std_auc = np.std(aucs)
-std_tpr = np.std(tprs, axis=0)
-tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
-tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
-plt.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2, label=r'$\pm$ 1 std. dev.')
 plt.xlim([-0.05, 1.05])
 plt.ylim([-0.05, 1.05])
 plt.xlabel('False Positive Rate')
