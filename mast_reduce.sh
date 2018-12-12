58332ab45a352cc55c105538aca7d409f608fcf2
Boris Shilov <21023619+borisofmuscovy@users.noreply.github.com>
2018-12-12 18:35:28 +0100

    deleted broken select_subset scripts

--- mast_reduce.sh
+++ mast_reduce.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
-echo "Running MAST matrix reduction..."
+echo "Running MAST matrix reduction on unbalanced data..."
 
 Rscript Mast_reduce.R feature_matrix_2.csv feature_matrix_2_mast_reduced.csv
 
 echo -e "\nYou should now run this matrix through Boruta to further reduce it."
+
+echo "Running MAST matrix reduction on balanced data..."
+
+Rscript Mast_reduce.R FMs/FM_orig_train.csv FMs/FM_mast_reduced_train.csv
+
+echo -e "\nYou should also run this matrix through Boruta to further reduce it."
